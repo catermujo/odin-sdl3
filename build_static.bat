cd64d9251a57c7fed21dcaff02a82dab9bc7f665
Isabella Basso <isabellabdoamaral@gmail.com>
2026-02-10 16:47:08 -0300

    Remove \\NUL from Windows build dir checks

--- build_static.bat
+++ build_static.bat
@@ -2,16 +2,16 @@
 
 setlocal EnableDelayedExpansion
 
-if not exist SDL\NUL (
+if not exist SDL (
    git clone https://github.com/libsdl-org/SDL --revision 0f061ff154dafb2f65fd882aa69beb119f99318d --depth=1 --recurse-submodules -j 4
 )
-if not exist SDL_image\NUL (
+if not exist SDL_image (
    git clone https://github.com/libsdl-org/SDL_image --revision 925c19db4d0bc9809fd3ac25c7e0ef771b668390 --depth=1 --recurse-submodules -j 10
 )
-if not exist SDL_mixer\NUL (
+if not exist SDL_mixer (
    git clone https://github.com/libsdl-org/SDL_mixer --revision 9cc531686cb3e3ed394790ff075c9760359c0756 --depth=1 --recurse-submodules -j 4
 )
-if not exist SDL_ttf\NUL (
+if not exist SDL_ttf (
    git clone https://github.com/libsdl-org/SDL_ttf --revision 6b1114c526ed98d5d03bf504ef06f04c669e2be2 --depth=1 --recurse-submodules -j 10
 )
 
